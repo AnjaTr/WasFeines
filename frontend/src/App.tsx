92c81371243bebfa554fed86358bc4f892ecac02
Georg Grab <g_grab@apple.com>
2025-03-08 16:40:47 +0000

    feat: a bit of styling , update readme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Rating from '@mui/material/Rating';
+import Card from '@mui/material/Card';
+
 
 const mediaItems = [
   { type: "image", src: "https://placehold.co/200x1000/000000/FFFFFF/png" },
@@ -30,12 +31,19 @@ const TransparentAppBar = () => {
 
 const MediaItem = ({ item }) => {
   return (
-    <Box sx={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+    <Box sx={{ width: "100vw", height: "100vh", overflow: "hidden", position: "relative" }}>
       {item.type === "image" ? (
         <img src={item.src} alt="Media" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
       ) : (
         <video src={item.src} controls autoPlay loop muted style={{ width: "100%", height: "100%", objectFit: "cover" }} />
       )}
+      <Card sx={{ position: "absolute", bottom: "3%", width: "85%", left: 0, right:0, margin: "auto", padding: "10px" }}>
+        <Typography variant="h6">Title</Typography>
+        <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+        <Typography variant="body1">Description</Typography>
+        <Typography variant="body1">Description</Typography>
+        <Typography variant="body1">Description</Typography>
+      </Card>
     </Box>
   );
 };
