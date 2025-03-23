6dfd6426e1f46020704a7628621580f9ff3c1466
Georg Grab <git@georggrab.net>
2025-03-23 18:44:16 +0000

    chore: logging config

--- api/src/wasfeines/app.py
+++ api/src/wasfeines/app.py
@@ -1,11 +1,15 @@
 from typing import List
 from contextlib import asynccontextmanager
+import logging
+import sys
 
 from fastapi import FastAPI, Request, APIRouter
 
 from wasfeines.models import Recipe
 from wasfeines.settings import Settings
 from wasfeines.storage.repository import S3StorageRepository
+
+log = logging.getLogger(__name__)
 
 api_v1_router = APIRouter()
 
@@ -14,10 +18,19 @@ async def get_recipes(request: Request) -> List[Recipe]:
     repo: S3StorageRepository = request.app.state.storage_repository
     return await repo.list_recipes()
 
+def configure_logging(settings: Settings):
+    log_level = logging.DEBUG if settings.debug else logging.INFO
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        stream=sys.stdout,
+    )
+
 @asynccontextmanager
 async def lifespan(app: FastAPI, settings: Settings):
     app.state.settings = settings
     app.state.storage_repository = S3StorageRepository(settings)
+    configure_logging(settings)
     yield
     print("Shutting down")
 
--- api/src/wasfeines/settings.py
+++ api/src/wasfeines/settings.py
@@ -11,5 +11,6 @@ class Settings(BaseSettings):
     s3_region: str = Field(alias='S3_REGION')
     s3_endpoint_url: str = Field(alias='S3_ENDPOINT_URL')
     s3_bucket_base_path: str = Field(alias='S3_BUCKET_BASE_PATH')
+    debug: bool = Field(alias='DEBUG', default=False)
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
